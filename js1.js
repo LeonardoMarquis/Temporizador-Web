61fdd431b5f1aa5bdcee35411834268f434a3bf3
LeonardoMarquis <marquisleonardo12@gmail.com>
2025-07-22 09:48:31 -0300

    audios 15 sec

--- js1.js
+++ js1.js
@@ -5,30 +5,89 @@ let count = 0;
 const value = document.querySelector('#value');
 const btns = document.querySelectorAll(".btn");
 
-//console.log(btns);
+let intervalID = null;
+
+
+const audio1 = document.querySelector("#audio1");   // pega pelo id "#id"
+const audio2 = document.querySelector("#audio2");
+const text_super = document.querySelector("#text-super");
+
+function UpdateColor(){
+    if (count > 0){
+        value.style.color = "green";
+    }
+    else if(count < 0){
+        value.style.color = "red";
+    }
+    else{
+        value.style.color = "#222";
+    }
+}
 
 btns.forEach(function(btn){
     btn.addEventListener("click", function (e){
         const styles = e.currentTarget.classList;
-        if (styles.contains("btn-decrease")){
-            count --;
+
+
+        if (styles.contains("btn-super")){
+            audio2.play();
+            text_super.style.display = "block";
         }
-        else if (styles.contains('btn-increase')){
+        else if (styles.contains("btn-decrease")){
+            count --;
+            clearInterval(intervalID);
+        }
+        else if (styles.contains("btn-increase")){
             count ++;
+            clearInterval(intervalID);
         }
         else if (styles.contains("btn-reset")){
             count = 0;
+            audio1.pause();
+            clearInterval(intervalID);
         }
 
-        if (count > 0){
-            value.style.color = "green";
+
+        else if (styles.contains("btn-increase+15")){
+            count += 15;
+            clearInterval(intervalID);
         }
-        else if(count < 0){
-            value.style.color = "red";
+        else if (styles.contains("btn-decrease-15")){
+            count -= 15;
+            clearInterval(intervalID);
         }
-        else{
-            value.style.color = "#222";
+
+
+        else if (styles.contains("btn-start")){
+            clearInterval(intervalID);
+            if (audio2.paused){     // retorna true se o audio 2 esta pausado/ nao tocando
+                audio1.play();
+            }
+
+            
+
+            intervalID = setInterval(() => {
+                if (count > 0){
+                    count --;
+
+                }else if (count < 0){
+                    count ++;
+                }
+
+                else{
+                    audio1.pause();
+                    clearInterval(intervalID);
+                }
+
+                UpdateColor();
+                value.textContent = count;
+
+            }, 1000);
+        
         }
+
+
+        UpdateColor();
         value.textContent = count;
     })
 })
